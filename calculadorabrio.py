b38eeb3445f0ec7db457b636e01f20e857386a84
Maria Eduarda Nichelle <95757504+mariaeduardanichelle@users.noreply.github.com>
2022-12-26 01:32:20 -0300

    Update calculadorabrio.py

--- calculadorabrio.py
+++ calculadorabrio.py
@@ -1,4 +1,5 @@
 #Calculadora de Briot-Ruffini
+import math
 raiz = int(input('Digite a raiz do polinômio: '))
 grau = int(input('Digite o grau do seu polinômio: '))
 if grau == 3:
@@ -11,6 +12,10 @@ if grau == 3:
     an0 = raiz * an1 + a
     if an0 == 0:
         print(f'{raiz} é uma raiz do polinômio {x3}x³{x2}x²{x1}x{a}')
+        #Determinando as outras raízes com a fórmula quadrática 
+        raiz2 = ((-an2)+ math.sqrt(an2**2-4*x3*an1))/(2*x3) 
+        raiz3 = ((-an2)- math.sqrt(an2**2-4*x3*an1))/(2*x3)
+        print(f'As raízes do polinômio {x3}x³{x2}x²{x1}x{a} são: {raiz},{raiz2},{raiz3}')
     else:
         print(f'{raiz} não é uma raiz do polinômio {x3}x³{x2}x²{x1}x{a}')
 else:
