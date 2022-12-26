28567503deb5b92cfcbeaf79b4e849cb0b268271
Maria Eduarda Nichelle <95757504+mariaeduardanichelle@users.noreply.github.com>
2022-12-26 16:13:42 -0300

    Update calculadorabrio.py

--- calculadorabrio.py
+++ calculadorabrio.py
@@ -16,6 +16,26 @@ if grau == 3:
         raiz2 = ((-an2)+ math.sqrt(an2**2-4*x3*an1))/(2*x3) 
         raiz3 = ((-an2)- math.sqrt(an2**2-4*x3*an1))/(2*x3)
         print(f'As raízes do polinômio {x3}x³{x2}x²{x1}x{a} são: {raiz},{raiz2},{raiz3}')
+        #Criando tabela do Briot-Ruffini
+        linha1 = [raiz, x3, x2, x1, a]
+        linha12 = [x3, an2, an1, an0]
+        print('{:^6}   {:^13}   {:^13}   {:^13}   {:^13}'.format(*linha1))
+        print('         {:^13}   {:^13}   {:^13}   {:^13}'.format(*linha12))
+        #Fazendo Briot-Ruffini para a raiz2
+        an22 = raiz2 * x3 + an2
+        an12 = raiz2 * an22 + an1
+        linha2 = [raiz2, x3, an2, an1]
+        linha22 = [x3, an22, an12]
+        #Criando tabela do Briot-Ruffini para a raiz2
+        print('{:^6}   {:^13}   {:^13}   {:^13}'.format(*linha2))
+        print('         {:^13}   {:^13}   {:^13}'.format(*linha22)) 
+        #Fazendo Briot-Ruffini para a raiz3
+        an23 = raiz3 * x3 + an22
+        linha3 = [raiz3, x3, an22]
+        linha32 = [x3, an23]
+        #Criando tabela do Briot-Ruffini para a raiz3
+        print('{:^6}   {:^13}   {:^13}'.format(*linha3))
+        print('         {:^13}   {:^13}'.format(*linha32)) 
     else:
         print(f'{raiz} não é uma raiz do polinômio {x3}x³{x2}x²{x1}x{a}')
 else:
